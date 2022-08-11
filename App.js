e05c5fa9dc225dd5a1b1c8a746991cc9b41f18f1
satyanarayana8861260047 <99236392+satyanarayana8861260047@users.noreply.github.com>
2022-08-11 16:06:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Signin from './components/Signin';
+
+const App = () => {
+  return (
+ <>
+ < Signin/>
+ </>
+);
+  
+}
+
+export default App;
